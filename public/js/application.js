8d8392e3d2b49fc791bafa29058f827a3e6b677c
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-10 19:46:28 -0500

    jquery ajax solution

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $("#get_color").on('click', function(event){
+    event.preventDefault();
+    $.post('/color', function(serverResponse){  
+      $('li').eq(serverResponse.cell - 1).css('background-color', serverResponse.color);
+      // $('ul li:nth-child(serverResponse.cell)')
+    });
+  });
 });
+
